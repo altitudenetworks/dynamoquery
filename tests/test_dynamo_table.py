d4fd3c9c0184698718b81358062bdfb9da2c3702
Vlad Emelianov <volshebnyi@gmail.com>
2020-02-20 15:47:58 +0100

    Add dynamotable tests

--- tests/test_dynamo_table.py
+++ tests/test_dynamo_table.py
@@ -1,7 +1,21 @@
+import datetime
 from unittest.mock import MagicMock
 
+import pytest
+
+from dynamo_query.data_table import DataTable
 from dynamo_query.dynamo_table import DynamoTable
 from dynamo_query.dynamo_table_index import DynamoTableIndex
+
+
+@pytest.fixture
+def _patch_datetime(monkeypatch):
+    class DatetimeMock:
+        @classmethod
+        def utcnow(cls):
+            return "utcnow"
+
+    monkeypatch.setattr(datetime, "datetime", DatetimeMock)
 
 
 class TestDynamoTableIndex:
@@ -22,6 +36,7 @@ class TestDynamoTableIndex:
         class MyDynamoTable(DynamoTable):
             global_secondary_indexes = [DynamoTableIndex("gsi", "gsi_pk", "gsi_sk")]
             local_secondary_indexes = [DynamoTableIndex("lsi", "lsi_pk", None)]
+            required_columns = {"pk", "sk"}
 
             @property
             def table(self):
@@ -82,5 +97,132 @@ class TestDynamoTableIndex:
             TableName="my_table_name",
         )
 
-    # def test_clear_table(self):
-    #     self.result.clear_table("pk_value")
+    def test_clear_table(self):
+        self.table_mock.query.return_value = {"Items": [{"pk": "my_pk", "sk": "sk"}]}
+        self.table_mock.scan.return_value = {"Items": [{"pk": "my_pk", "sk": "sk"}]}
+        self.result.clear_table("my_pk")
+        self.table_mock.query.assert_called_with(
+            ConsistentRead=False,
+            ExpressionAttributeNames={"#aaa": "pk", "#aab": "sk"},
+            ExpressionAttributeValues={":aaa": "my_pk"},
+            KeyConditionExpression="#aaa = :aaa",
+            Limit=1000,
+            ProjectionExpression="#aaa, #aab",
+            ScanIndexForward=True,
+        )
+        self.client_mock.batch_write_item.assert_called_with(
+            RequestItems={
+                "my_table_name": [
+                    {"DeleteRequest": {"Key": {"pk": "my_pk", "sk": "sk"}}}
+                ]
+            },
+            ReturnConsumedCapacity="NONE",
+            ReturnItemCollectionMetrics="NONE",
+        )
+
+        self.result.clear_table(None)
+        self.table_mock.scan.assert_called_with(
+            ExpressionAttributeNames={"#aaa": "pk", "#aab": "sk"},
+            Limit=1000,
+            ProjectionExpression="#aaa, #aab",
+        )
+        self.client_mock.batch_write_item.assert_called_with(
+            RequestItems={
+                "my_table_name": [
+                    {"DeleteRequest": {"Key": {"pk": "my_pk", "sk": "sk"}}}
+                ]
+            },
+            ReturnConsumedCapacity="NONE",
+            ReturnItemCollectionMetrics="NONE",
+        )
+
+        self.table_mock.scan.return_value = {"Items": []}
+        self.result.clear_table(None)
+
+    def test_batch_get(self):
+        self.client_mock.batch_get_item.return_value = {
+            "Responses": {
+                "my_table_name": [{"pk": "my_pk", "sk": "my_sk", "data": "value"}]
+            }
+        }
+        data_table = DataTable().add_record({"pk": "my_pk", "sk": "my_sk"})
+        assert list(self.result.batch_get(data_table).get_records()) == [
+            {"pk": "my_pk", "sk": "my_sk", "data": "value"}
+        ]
+        self.client_mock.batch_get_item.assert_called_with(
+            RequestItems={"my_table_name": {"Keys": [{"pk": "my_pk", "sk": "my_sk"}]}},
+            ReturnConsumedCapacity="NONE",
+        )
+
+        assert list(self.result.batch_get(DataTable()).get_records()) == []
+
+        with pytest.raises(ValueError):
+            self.result.batch_get(DataTable().add_record({"pk": "my_pk"}))
+
+    def test_batch_delete(self):
+        self.client_mock.batch_write_item.return_value = {
+            "Responses": {
+                "my_table_name": [{"pk": "my_pk", "sk": "my_sk", "data": "value"}]
+            }
+        }
+        data_table = DataTable().add_record({"pk": "my_pk", "sk": "my_sk"})
+        assert list(self.result.batch_delete(data_table).get_records()) == [
+            {"pk": "my_pk", "sk": "my_sk"}
+        ]
+        self.client_mock.batch_write_item.assert_called_with(
+            RequestItems={
+                "my_table_name": [
+                    {"DeleteRequest": {"Key": {"pk": "my_pk", "sk": "my_sk"}}}
+                ]
+            },
+            ReturnConsumedCapacity="NONE",
+            ReturnItemCollectionMetrics="NONE",
+        )
+
+        assert list(self.result.batch_delete(DataTable()).get_records()) == []
+
+        with pytest.raises(ValueError):
+            self.result.batch_delete(DataTable().add_record({"pk": "my_pk"}))
+
+    def test_batch_upsert(self, _patch_datetime):
+        self.client_mock.batch_write_item.return_value = {
+            "Responses": {
+                "my_table_name": [{"pk": "my_pk", "sk": "my_sk", "data": "value1"}]
+            }
+        }
+        data_table = DataTable().add_record(
+            {"pk": "my_pk", "sk": "my_sk", "data": "value1"}
+        )
+        assert list(self.result.batch_upsert(data_table).get_records()) == [
+            {
+                "pk": "my_pk",
+                "sk": "my_sk",
+                "data": "value1",
+                "dt_created": "utcnow",
+                "dt_modified": "utcnow",
+            }
+        ]
+        self.client_mock.batch_write_item.assert_called_with(
+            RequestItems={
+                "my_table_name": [
+                    {
+                        "PutRequest": {
+                            "Item": {
+                                "pk": "my_pk",
+                                "sk": "my_sk",
+                                "data": "value1",
+                                "dt_created": "utcnow",
+                                "dt_modified": "utcnow",
+                            }
+                        }
+                    }
+                ]
+            },
+            ReturnConsumedCapacity="NONE",
+            ReturnItemCollectionMetrics="NONE",
+        )
+
+        assert list(self.result.batch_upsert(DataTable()).get_records()) == []
+
+        with pytest.raises(ValueError):
+            self.result.batch_upsert(DataTable().add_record({"pk": "my_pk"}))
