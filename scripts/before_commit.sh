f9fcc9769b61456862c36389d9bdb19db8f2c256
Vlad Emelianov <volshebnyi@gmail.com>
2020-02-21 20:14:07 +0100

    Add boto3 stubs

--- scripts/before_commit.sh
+++ scripts/before_commit.sh
@@ -4,8 +4,6 @@ set -e
 ROOT_PATH=$(dirname $(dirname $(realpath $0)))
 cd $ROOT_PATH
 
-python -m mypy_boto3
-
 # vulture dynamo_query --make-whitelist > vulture_whitelist.txt
 vulture dynamo_query vulture_whitelist.txt
 mypy dynamo_query
