07cdf252c1e7b6d86f7254c662d04ed74ab17d22
Vlad Emelianov <volshebnyi@gmail.com>
2020-02-23 00:47:41 +0100

    Add required -release postfix

--- scripts/release.sh
+++ scripts/release.sh
@@ -3,7 +3,7 @@ set -e
 
 ROOT_PATH=$(dirname $(dirname $(realpath $0)))
 
-rm -rf build *.egg-info dist/* > /dev/null
-python setup.py build bdist_wheel 1>/dev/null 2>/dev/null
-twine upload --non-interactive dist/* > /dev/null || true
-rm -rf build *.egg-info dist/* > /dev/null
+rm -rf build *.egg-info dist/*
+python setup.py build bdist_wheel
+twine upload --non-interactive dist/*
+rm -rf build *.egg-info dist/*
