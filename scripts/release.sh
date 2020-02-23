bfe4bfe3152b7437313d2055a3091c415d636e36
Vlad Emelianov <volshebnyi@gmail.com>
2020-02-23 15:46:58 +0100

    Move all logic to actions

--- scripts/release.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-ROOT_PATH=$(dirname $(dirname $(realpath $0)))
-
-rm -rf build *.egg-info dist/*
-python setup.py build bdist_wheel
-twine upload --non-interactive dist/*
-rm -rf build *.egg-info dist/*
