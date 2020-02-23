fa0f35e55a4cb4397da62f2c4f04c1ab34bb9302
Vlad Emelianov <volshebnyi@gmail.com>
2020-02-23 15:53:05 +0100

    Add debug to docs build

--- scripts/docs.sh
+++ scripts/docs.sh
@@ -4,4 +4,4 @@ set -e
 ROOT_PATH=$(dirname $(dirname $(realpath $0)))
 cd $ROOT_PATH
 
-handsdown --external `git config --get remote.origin.url` -n dynamo-query dynamo_query --exclude '*/build/*' --cleanup --panic
+handsdown --external `git config --get remote.origin.url` -n dynamo-query dynamo_query --exclude '*/build/*' --cleanup --panic $@
