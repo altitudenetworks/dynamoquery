68f598696acb5b6dcbf64a064b8dff216c820401
Vlad Emelianov <volshebnyi@gmail.com>
2020-06-27 01:04:36 +0200

    Fix computed tests

--- tests/dictclasses/test_loose_dictclass.py
+++ tests/dictclasses/test_loose_dictclass.py
@@ -7,19 +7,22 @@ class MyLooseDictClass(LooseDictClass):
     required: int
     value: int = 5
 
+    @LooseDictClass.compute_key("computed")
+    def get_computed(self) -> str:
+        return "test"
+
 
 class TestLooseDictClass:
     def test_init(self):
         result = MyLooseDictClass(required=1, new="test")
-        assert result == {
-            "new": "test",
-            "required": 1,
-            "value": 5,
-        }
+        assert result == {"new": "test", "required": 1, "value": 5, "computed": "test"}
 
         result["new"] = "asd"
         result["newest"] = "asd"
         result.update({"other": "test"})
+
+        with pytest.raises(KeyError):
+            result["computed"] = "new"
 
         with pytest.raises(ValueError):
             MyLooseDictClass(new="test")
